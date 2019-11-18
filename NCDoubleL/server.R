732e4e8257f377a231ad9f8f0b0d6a4a6c1c9fbd
Victoria Jiang <jiangs17@uw.edu>
2019-11-18 14:03:08 -0800

    updated shiny code

--- NCDoubleL/server.R
+++ NCDoubleL/server.R
@@ -2,18 +2,4 @@
 
 library(shiny)
 
-server <- function(input, output) {
-  output$page1 <- renderUI ({})
-  
-  output$page2 <- renderUI ({})
-  
-  output$page3 <- renderUI ({})
-  
-  output$page4 <- renderUI ({})
-  
-  output$page5 <- renderUI ({})
-  
-  output$page6 <- renderUI ({})
-}
-
-shinyServer(server)
+server <- function(input, output) {}
--- NCDoubleL/ui.R
+++ NCDoubleL/ui.R
@@ -4,16 +4,15 @@ ui <- fluidPage(
   titlePanel("United States Cancer"),
   
     navbarPage("Cancer Information",
-               tabPanel("Overview", uiOutput('page1')),
-               tabPanel("Research Question", uiOutput('page2')),
+               tabPanel("Overview"),
+               tabPanel("Research Question"),
                navbarMenu("Visualization Maps",
-                          tabPanel("Visualization #1", uiOutput('page3')),
-                          tabPanel("Visualization #2", uiOutput('page4'))
+                          tabPanel("Visualization #1"),
+                          tabPanel("Visualization #2")
                           ),
-               tabPanel("Conclusion", uiOutput('page5')),
-               tabPanel("About Us", uiOutput('page6'))
+               tabPanel("Conclusion"),
+               tabPanel("About Us")
                )
  
 )
 
-shinyUI(ui)
